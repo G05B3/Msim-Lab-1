5d2e6e0893d069ee09f1b421d9d05c61ee7a6ddc
FilipeJInacio <102881633+FilipeJInacio@users.noreply.github.com>
2022-11-29 15:44:26 +0000

    Update Lab1_P2_2.m

--- Lab1_P2_2.m
+++ Lab1_P2_2.m
@@ -1,4 +1,3 @@
-
 n_it = 150;
 h = 1;
 v1 = 3110;
@@ -14,26 +13,25 @@ c50=7.1903;
 c2(1:n_it)=zeros;
 u(1:n_it+1)=zeros;
 du(1:n_it+1)=zeros;
-d=(1:1:n_it+1);
+d=(0:3:n_it*3);
 
 
 
 for k=1:n_it
     c2(k)=k12/(k10*k21)*delta*d(k);
     u(k)=c2(k)/(c2(k)+c50);
-    du(k+1)=(u(k+1) - u(k))/h ;  
+    if k~=1
+        du(k)=(u(k) - u(k-1))/h ;
+    end
 end
 
 
 figure(1)
-plot(d(1:n_it),u(1:n_it))  % u(t)
+plot(d(2:n_it),u(2:n_it))  % u(t)
 hold on
 
-plot(d,du)  % du(t)
+plot(d(2:n_it),du(2:n_it))  % du(t)
 hold on
-
-
-
 
 
 
