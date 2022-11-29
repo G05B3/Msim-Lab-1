de719600b9918fd6216bec6616582e41f175104d
FilipeJInacio <102881633+FilipeJInacio@users.noreply.github.com>
2022-11-29 14:43:18 +0000

    Update Lab1_P2_2.m

--- Lab1_P2_2.m
+++ Lab1_P2_2.m
@@ -12,30 +12,25 @@ c50=7.1903;
 
 
 c2(1:n_it)=zeros;
-u(1:n_it)=zeros;
-du(1:n_it)=zeros;
-d=(1:1:n_it);
+u(1:n_it+1)=zeros;
+du(1:n_it+1)=zeros;
+d=(1:1:n_it+1);
 
 
 
 for k=1:n_it
     c2(k)=k12/(k10*k21)*delta*d(k);
     u(k)=c2(k)/(c2(k)+c50);
-    du(k+1)= u(k);  % dont know how to do it
+    du(k+1)=(u(k+1) - u(k))/h ;  
 end
 
 
 figure(1)
-plot(d,u)  % u(t)
+plot(d(1:n_it),u(1:n_it))  % u(t)
 hold on
 
-
-
-
-
-
-
-
+plot(d,du)  % du(t)
+hold on
 
 
 
